6633acb534f158b279d443d1a5acd642b1b55cea
tavusB <35615283+tavusB@users.noreply.github.com>
2020-07-20 20:34:38 -0500

    commit "deploy"

---
+++ jenkinsDeploy.groovy
@@ -0,0 +1,9 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply the changes', name: 'terraformApply'),
+        booleanParam(defaultValue: false, description: 'Please select to destroy everything.', name: 'terraformDestroy'),
+        booleanParam(defaultValue: false, description: 'Please select to run the job in debug mode', name: 'debugMode'),
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please select the environment to deploy.', name: 'environment')
+        ])
+    ])
+println('Hello world')
