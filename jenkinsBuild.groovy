31f284a859f87cef4ba2e31e9fd6b9dc28955834
tavusB <35615283+tavusB@users.noreply.github.com>
2020-07-24 14:08:36 -0500

    new changes

--- jenkinsBuild.groovy
+++ jenkinsBuild.groovy
@@ -70,7 +70,9 @@ def slavePodTemplate = """
                 }
 
                 stage("Docker Login") {
-                    withCredentials([usernamePassword(credentialsId: 'docker credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
+                    
+                    
+                    withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'password', usernameVariable: 'username')]) {
                       sh "docker login --username ${username} --password ${password}"
                     }
                 }
